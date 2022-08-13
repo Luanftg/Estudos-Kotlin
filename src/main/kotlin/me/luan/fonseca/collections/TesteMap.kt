2787c5c3308117584dc9facdfce722b67a1e816e
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-08-12 23:08:34 -0300

    testes com map

---
+++ src/main/kotlin/me/luan/fonseca/collections/TesteMap.kt
@@ -0,0 +1,15 @@
+﻿package me.luan.fonseca.collections
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("Luan", 1200.00)
+    val map1 = mapOf(pair)
+
+    map1.forEach { (t, u) -> println("Chave: $t - Valor: $u") }
+
+    val map2 = mapOf(
+        "Lena" to 2100.00,
+        "Luiza" to 1500.00
+    )
+
+    map2.forEach { (t, u) -> println("Chave: $t - Valor: $u")  }
+}
